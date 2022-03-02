c9ac8346955097f023262217df3bf1e03c061a5c
jacksonnorris <jacksonnorr@gmail.com>
2022-03-02 12:30:43 -0500

    v1 styling for forms

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $('#currentDay').text(moment().format('dddd MMMM Do'));
 var currentHour = parseInt(moment().format('HH'));
 console.log(currentHour);
-$('.time-block>input').each(function(index) {
+$('.time-block').each(function(index) {
     console.log(index);
     if (index + 9 < currentHour) { this.classList.add('past'); }
     else if (index + 9 === currentHour) { this.classList.add('present'); }

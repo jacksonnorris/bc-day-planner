a7bd7620d4e58a00fe8e9097d2b98c2804a010b7
jacksonnorris <jacksonnorr@gmail.com>
2022-03-02 23:14:22 -0500

    added functionality to all divs, clicking save btn still doesn't work

--- script.js
+++ script.js
@@ -1,39 +1,18 @@
 $('#currentDay').text(moment().format('dddd MMMM Do'));
 var currentHour = parseInt(moment().format('HH'));
-console.log(currentHour);
-
 $('.form-horizontal').on('click', '.saveBtn', storeData)
-
+$('.form-horizontal').on('click', '.fa-save', function() { console.log('click'); $(this).parent().click();} )
 $('.time-block').each(function(index) {
-    console.log(index);
     if (index + 9 < currentHour) { this.classList.add('past'); }
     else if (index + 9 === currentHour) { this.classList.add('present'); }
     else { this.classList.add('future'); }
 })
 var localData = JSON.stringify(localStorage.getItem('9am'));
-console.log("local data", localData)
-console.log("local storage", localStorage)
 for (const [key, value] of Object.entries(localStorage)) {
-    console.log(key, value);
     $('#' + key).attr('value', value);
  }
 function storeData(event) {
-    // event.preventDefault()
-    // console.log(time);
-    // var formDiv = time.children("div");
-    // console.log(formDiv);
-    // var inputGroup = formDiv.children("div");
-    // console.log(inputGroup);
-    // var formInput = inputGroup.children("input");
-    // console.log(formInput);
-    // console.log(formInput[0]);
-    // var data = $('#' + time).val();
     var currentTag = $(event.target).siblings(".hour").text().toLowerCase();
     var data = $('#' + currentTag).val();
-    console.log(data);
-    // alert(data);
-    // console.log(data);
-    
-    
     localStorage.setItem(currentTag, data);
 }
